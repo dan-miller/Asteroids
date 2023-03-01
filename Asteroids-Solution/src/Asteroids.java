f926a2935bc1bd3812ad1d7dfefba990b1d1bb01
Mr. Miller <daniel_miller@charleston.k12.sc.us>
2023-03-01 09:56:12 -0500

    some stuff

--- Asteroids-Solution/src/Asteroids.java
+++ Asteroids-Solution/src/Asteroids.java
@@ -15,6 +15,18 @@ public class Asteroids extends Game {
 	public void paint(Graphics brush) {
     brush.setColor(Color.black);
     brush.fillRect(0,0,width,height);
+    brush.setColor(Color.white);
+
+    // GET MOUSE POSITION
+    java.awt.Point mousePos = this.getMousePosition();
+    int xPos = mousePos != null ? mousePos.x : -1;
+
+    
+    brush.drawString("X: " + this.getMousePosition().x + " Y: " + this.getMousePosition().y, 200, 200);
+
+    Point[] shipPoints = {new Point(0,0), new Point(50, 50), new Point(0, 50)};
+    Ship ship = new Ship(shipPoints, new Point(300,300), 15);
+    ship.paint(brush);
   }
   
 	public static void main (String[] args) {
--- Asteroids-Solution/src/Ship.java
+++ Asteroids-Solution/src/Ship.java
@@ -6,7 +6,16 @@ public class Ship extends Polygon {
     }
 
     public void paint(Graphics brush) {
-        brush.fillPolygon(null, null, 0);
+        int[] xPositions = new int[3];
+        int[] yPositions = new int[3];
+        xPositions[0] = (int)this.getPoints()[0].x;
+        xPositions[1] = (int)this.getPoints()[1].x;
+        xPositions[2] = (int)this.getPoints()[2].x;
+        yPositions[0] = (int)this.getPoints()[0].y;
+        yPositions[1] = (int)this.getPoints()[1].y;
+        yPositions[2] = (int)this.getPoints()[2].y;
+        brush.setColor(Color.WHITE);
+        brush.fillPolygon(xPositions, yPositions, 3);
     }
     
 }
