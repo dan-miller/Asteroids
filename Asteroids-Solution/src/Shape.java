daf77905bd2c7e237e16e8019d57a3453dc907c8
Daniel Miller <daniel_miller@charleston.k12.sc.us>
2023-03-03 15:31:45 -0500

    wheeeee

--- Asteroids-Solution/src/Shape.java
+++ Asteroids-Solution/src/Shape.java
@@ -15,7 +15,20 @@ public abstract class Shape {
     }
 
     public void move(double velocityFactor) {
-        
+        this.position.x += (velocityFactor * Math.cos(Math.toRadians(this.heading)));
+        this.position.y += (velocityFactor * Math.sin(Math.toRadians(this.heading)));
+        if (this.position.x > 800) {
+            this.position.x = 0;
+        }
+        if (this.position.x < 0) {
+            this.position.x = 800;
+        }
+        if (this.position.y > 600) {
+            this.position.y = 0;
+        }
+        if (this.position.y < 0) {
+            this.position.y = 600;
+        }
     } 
 
 }
